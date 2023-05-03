6aad3c990804c13991cb393947051c8c5a2d7e27
Mikort <36812480+Mikort@users.noreply.github.com>
2023-05-03 21:24:30 +0500

    Added storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,38 @@
+import type {Preview} from "@storybook/react";
+import "../src/color-schema.module.css";
+import {BrowserRouter} from "react-router-dom";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story: any) => (
+      <BrowserRouter>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={""}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap"
+          rel="stylesheet"
+        />
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
+
+export default preview;
