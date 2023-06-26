1b3f8d4f07e2979e57ea2c2967696774d0d0b64f
Mikort <36812480+Mikort@users.noreply.github.com>
2023-06-26 20:12:33 +0500

    Fixed Card types for intelliSense and added id

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,9 @@
 import styleClasses from "./Card.module.css";
 import block from "bem-css-modules";
 import { BaseProps } from "src/BaseProps.interface";
-import { memo, ReactElement, useMemo } from "react";
+import React, { memo, ReactElement, useMemo } from "react";
 import { addRootNodeClassName } from "utils/addRootNodeClassName";
+import { mixClasses } from "utils/mixClasses";
 
 const classes = block(styleClasses);
 
@@ -26,24 +27,25 @@ export interface CardProps extends BaseProps {
 /**
  * Карточка с текстовой информацией
  */
-export const Card = memo<CardProps>(function ({
-  icon,
-  header,
-  description,
-  testId,
-}) {
-  const iconModified = useMemo(
-    () => addRootNodeClassName(icon, classes("icon")),
-    [icon]
-  );
+export const Card: React.FunctionComponent<CardProps> = memo<CardProps>(
+  function ({ icon, header, description, id, testId = "Card", className }) {
+    const iconModified = useMemo(
+      () => addRootNodeClassName(icon, classes("icon")),
+      [icon]
+    );
 
-  return (
-    <div className={classes()} data-testid={testId}>
-      {iconModified}
-      <article className={classes("article")}>
-        <h3 className={classes("header")}>{header}</h3>
-        <p className={classes("description")}>{description}</p>
-      </article>
-    </div>
-  );
-});
+    return (
+      <div
+        id={id}
+        className={mixClasses(classes(), className)}
+        data-testid={testId}
+      >
+        {iconModified}
+        <article className={classes("article")}>
+          <h3 className={classes("header")}>{header}</h3>
+          <p className={classes("description")}>{description}</p>
+        </article>
+      </div>
+    );
+  }
+);
